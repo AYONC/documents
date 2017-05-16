6351093b8c4b0215a529f999d913e2ddb8ed583a
최아연 <ayon.c@ridi.com>
2017-05-16 23:00:39 +0900

    update reveal.js latest

--- slide/reveal.js-3.5.0/custom_init.js
+++ slide/reveal.js-3.5.0/custom_init.js
@@ -94,27 +94,27 @@ Reveal.initialize({
 	// More info https://github.com/hakimel/reveal.js#dependencies
 	dependencies: [
         // Cross-browser shim that fully implements classList - https://github.com/eligrey/classList.js/
-        { src: '../../reveal.js-3.3.0/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        { src: '../../reveal.js-3.5.0/lib/js/classList.js', condition: function() { return !document.body.classList; } },
 
         // Interpret Markdown in <section> elements
-        { src: '../../reveal.js-3.3.0/plugin/markdown/marked.js',
+        { src: '../../reveal.js-3.5.0/plugin/markdown/marked.js',
         // condition: function() { return !!document.querySelector( '[data-markdown]' ); }
         },
-        { src: '../../reveal.js-3.3.0/plugin/markdown/markdown.js',
+        { src: '../../reveal.js-3.5.0/plugin/markdown/markdown.js',
         // condition: function() { return !!document.querySelector( '[data-markdown]' ); }
         },
 
         // Syntax highlight for <code> elements
-        { src: '../../reveal.js-3.3.0/plugin/highlight/highlight.pack.js', async:true, callback: function() { hljs.initHighlightingOnLoad(); }
+        { src: '../../reveal.js-3.5.0/plugin/highlight/highlight.pack.js', async:true, callback: function() { hljs.initHighlightingOnLoad(); }
         },
 
         // Zoom in and out with Alt+click
-        { src: '../../reveal.js-3.3.0/plugin/zoom-js/zoom.js', async: true }
+        { src: '../../reveal.js-3.5.0/plugin/zoom-js/zoom.js', async: true }
 
         // Speaker notes
-        // { src: '../../reveal.js-3.3.0/plugin/notes/notes.js', async: true },
+        // { src: '../../reveal.js-3.5.0/plugin/notes/notes.js', async: true },
 
         // MathJax
-        // { src: '../../reveal.js-3.3.0/plugin/math/math.js', async: true }
+        // { src: '../../reveal.js-3.5.0/plugin/math/math.js', async: true }
 	]
 });
